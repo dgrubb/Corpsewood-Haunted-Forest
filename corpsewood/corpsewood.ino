caa2ca8f7590ccfe23cab78b3cc3c5bdae5cec5b
dgrubb <davidanthonygrubb@gmail.com>
2017-10-16 11:12:50 -0500

    Finish up main logic
    1) Add a mapping between input and output pins.
    2) Prepend Catalex defines with CATALEX_ for rudimentary namespacing.
    3) Add a function for reading input states of pins which aren't currently driving a relay (outputing HIGH).
    4) Add a function for clearing a set relay after a set amount of time.
    5) Setup an interrupt pin for triggering MP3 playback.

--- corpsewood/corpsewood.ino
+++ corpsewood/corpsewood.ino
@@ -3,31 +3,101 @@
 #define APP_NAME    "Corpsewood Haunt Forest"
 #define APP_VERSION "0.1"
 
-#define ARDUINO_RX      10
-#define ARDUINO_TX      11
-#define PLAYLIST_LEN    3
+#define CATALEX_ARDUINO_RX      5
+#define CATALEX_ARDUINO_TX      6
+#define CATALEX_PLAYLIST_LEN    3
+#define CATALEX_INTERRUPT_PIN   2
+
+#define DELAY_TIME              100 // ms
 
 Catalex player();
 
-void setup() {
+typedef struct {
+    int input,
+    int output,
+    int timeout, // in increments of 100ms. E.g., for 1 second, timeout = 10
+    int count,
+} pin_map_t;
+
+pin_map_t IO_MAP[] = {
+    { A0, 8,  10, 0},
+    { A1, 9,  10, 0},
+    { A2, 10, 10, 0},
+    { A3, 11, 10, 0},
+    {NULL, NULL, NULL}
+}
+
+void setup()
+{
+    int i;
+
     Serial.begin(9600);
     while (!Serial) {
         // Wait for hardware serial to become available
     }
+
+    // Initialise I/O pin mapping
+    Serial.println("Mapping I/O");
+    i = 0;
+    while (NULL != IO_MAP[i].input) {
+        pinMode(IO_MAP[i].input, INPUT);
+        pinMode(IO_MAP[i].output, OUTPUT);
+        i++;
+    }
+
+    // Startup Catalex module
     Serial.println("Starting Catalex module");
     // I don't know exactly how long it takes the Catalex hardware module to 
     // power up and become responsive over UART, so give it half a second (a lifetime
     // in uC years!) to sort itself out
     delay(500); // ms
-    player.init(PLAYLIST_LEN, ARDUINO_RX, ARDUINO_TX);
+    player.init(CATALEX_PLAYLIST_LEN, CATALEX_ARDUINO_RX, CATALEX_ARDUINO_TX);
     while (!player.ready()) {
         // Wait for acknowledgement that hardware module is ready
     }
+    // Attach an interrupt to trigger playback
+    attachInterrupt(digitalPinToInterrupt(CATALEX_INTERRUPT_PIN), playClip, RISING);
+
+    // Fin.
     Serial.println(String(APP_NAME + ", " + APP_VERSION + " initialised"));
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    readInputs();
+    delay(DELAY_TIME);
+    updateOutputs();
+}
+
+void readInputs()
+{
+    int i = 0;
+    while (NULL != IO_MAP[i].input) {
+        // If a timeout delay has been set then it doesn't matter what
+        // state the relevant input pin is in, we can ignore it
+        if (0 == IO_MAP[i].count) {
+            if (digitalRead(IO_MAP[i])) {
+                IO_MAP[i].count = IO_MAP[i].timeout;
+                digitalWrite(IO_MAP[i].output HIGH);
+            }
+        }
+        i++;
+    }
+}
+
+void updateOutputs()
+{
+    int i = 0;
+    int val = 0;
+    while (NULL != IO_MAP[i].input) {
+        if (1 == IO_MAP[i].count) {
+            digitalWrite(IO_MAP[i].output, LOW);
+        }
+        if (0 != IO_MAP[i].count) {
+            IO_MAP[i].count--;
+        }
+        i++;
+    }
 }
 
 void playClip()
